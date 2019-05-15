8a7c5230918495fe3256fa0b2787792e3e0fe315
Riley Weber <rileyw13@protonmail.com>
2019-05-15 09:24:07 -0400

    added commented line that will allow me to use my own views and none of the spree frontend

--- config/initializers/spree.rb
+++ config/initializers/spree.rb
@@ -14,6 +14,7 @@ Spree.config do |config|
   # Uncomment to stop tracking inventory levels in the application
   # config.track_inventory_levels = false
   config.logo = 'logo.png'
+  # config.layout = 'application'
 end
 
 # Configure Spree Dependencies
