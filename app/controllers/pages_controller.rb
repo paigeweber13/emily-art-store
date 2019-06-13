6705c0da50ae9265affaed2ed5d899f91a82c6d8
Coulter <ecoulte1@uncc.edu>
2019-06-13 16:12:10 -0400

    Added contact and about pages with routes.

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -1,4 +1,8 @@
 class PagesController < ApplicationController
   def store
   end
+  def contact
+  end
+  def about
+  end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,6 @@
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
   root to: 'pages#store'
+  get 'contact', to: 'pages#contact'
+  get 'about', to: 'pages#about'
 end
