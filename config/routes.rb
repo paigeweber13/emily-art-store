06faa60006383b6740ed52e9c404eda7e9ba70f5
Coulter <ecoulte1@uncc.edu>
2019-07-23 13:27:35 -0400

    made sebsection for item pages

--- config/routes.rb
+++ config/routes.rb
@@ -3,5 +3,6 @@ Rails.application.routes.draw do
   root to: 'pages#store'
   get 'contact', to: 'pages#contact'
   get 'about', to: 'pages#about'
+  get 'meandmydog', to: 'store#meandmydog'
   get 'flowering', to: 'pages#flowering'
 end
